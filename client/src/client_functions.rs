fe9818c93b21e9a07b673f25136a215d84223caa
bacowan <bacowan@ualberta.ca>
2024-07-06 19:22:30 +0900

    temp

--- client/src/client_functions.rs
+++ client/src/client_functions.rs
@@ -6,7 +6,7 @@ use uuid::Uuid;
 use server::{ rest_bodies, rest_responses };
 
 
-pub async fn get_all_orders(table_number: String) -> Result<Vec<rest_responses::Order>, String> {
+pub async fn get_all_orders(table_number: u32) -> Result<Vec<rest_responses::Order>, String> {
     let web_response = reqwest::get(format!("tables/{table_number}/orders"))
         .await.map_err(|e| e.to_string())?;
 
@@ -21,7 +21,7 @@ pub async fn get_all_orders(table_number: String) -> Result<Vec<rest_responses::
     }
 }
 
-pub async fn get_order(order_id: String) -> Result<rest_responses::Order, String> {
+pub async fn get_order(table_number: u32, order_id: u32) -> Result<rest_responses::Order, String> {
     let web_response = reqwest::get(format!("orders/{order_id}"))
         .await.map_err(|e| e.to_string())?;
 
@@ -36,7 +36,7 @@ pub async fn get_order(order_id: String) -> Result<rest_responses::Order, String
     }
 }
 
-pub async fn add_orders<F>(host: String, table_number: String, menu_item_ids: Vec<u32>, should_retry: F) -> Result<(), String>
+pub async fn add_orders<F>(host: String, table_number: u32, menu_item_ids: Vec<u32>, should_retry: F) -> Result<Vec<rest_responses::Order>, String>
                         where F: Fn() -> bool {
     let orders = rest_bodies::Orders {
         orders: menu_item_ids.iter().map(|i| rest_bodies::Order {
@@ -64,16 +64,16 @@ pub async fn add_orders<F>(host: String, table_number: String, menu_item_ids: Ve
 
     match web_response.status() {
         StatusCode::OK => {
-            web_response.text()
-                .await
-                .map_err(|e| e.to_string())?;
-            Ok(())
+            let body = web_response.text()
+                .await.map_err(|e| e.to_string())?;
+            from_str::<Vec<rest_responses::Order>>(&body)
+                .map_err(|e| e.to_string())
         },
         status => Result::Err(status.to_string())
     }
 }
 
-pub fn delete_order(table_id: String, order_id: String) -> Result<(), String> {
+pub fn delete_order(table_number: u32, order_id: u32) -> Result<(), String> {
     return Result::Ok(());
 }
 
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,221 +1,156 @@
+use std::future::Future;
+use futures::executor::block_on;
 use std::io::{self, Write};
-use tokio;
 use server::rest_bodies;
-use server::rest_responses;
+use server::rest_responses::{self, MenuItem};
+use std::thread;
+use std::time::Duration;
+use rand::Rng;
+use rand::seq::SliceRandom;
 
 mod client_functions;
 
 const HOST: &str = "http://127.0.0.1:8000";
+const TABLE_COUNT: u32 = 5;
+const TABLET_COUNT: u32 = 1;
+const RUN_TIME_MILLIS: u64 = 60000; // 1 minute
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    match client_functions::get_menu_items(HOST.to_string()).await {
-        Result::Ok(items) => main_loop(items.menu_items).await,
-        Result::Err(e) => println!("Failed retrieving menu items: {e}")
-    };
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    for i in 1..TABLET_COUNT + 1 {
+        thread::spawn( move || block_on(client_tablet(i)));
+    }
 
+    thread::sleep(Duration::from_millis(RUN_TIME_MILLIS));
     Result::Ok(())
 }
 
-async fn main_loop(menu_items: Vec<rest_responses::MenuItem>) {
+async fn client_tablet(client_number: u32) {
+    let mut added_items: Vec<(u32,u32)> = Vec::new();
     loop {
-        println!("Type one of the following commands to continue:");
-        println!("  1: List orders");
-        println!("  2: Add orders");
-        println!("  3: Delete order");
-        println!("  exit: Exit the application");
-
-        let command = get_user_input();
-
-        match command.trim() {
-            "1" => list_orders(),
-            "2" => add_orders(&menu_items).await,
-            "3" => delete_order(),
-            "exit" => return,
-            _ => println!("Invalid command")
+        thread::sleep(Duration::from_millis(rand::thread_rng().gen_range(1000..5000)));
+        match rand::thread_rng().gen_range(1..5) {
+            1 => add_random_order(client_number, &mut added_items).await,
+            2 => delete_random_order(client_number, &mut added_items).await,
+            3 => query_random_table(client_number).await,
+            _ => query_random_table_item(client_number, &added_items).await
         }
     }
 }
 
-fn list_orders() {
-    println!("Input a table number, or type \"exit\" to go back.");
-    loop {
-        let table_number = get_user_input();
-        match table_number.trim() {
-            "" => {},
-            "exit" => return,
-            n => {
-                list_orders_for_table(n.to_string());
-                return;
-            }
-        }
-    }
+async fn add_random_order(client_number: u32, added_items: &mut Vec<(u32,u32)>) {
+    let table_number = rand::thread_rng().gen_range(1..TABLE_COUNT + 1);
+    let menu_item_names = (0..rand::thread_rng().gen_range(1..3))
+        .map(|_| match rand::thread_rng().gen_range(1..6) {
+            1 => "Hamburger".to_string(),
+            2 => "Salad".to_string(),
+            3 => "Sushi".to_string(),
+            4 => "Ice Cream".to_string(),
+            _ => "Soda".to_string()
+        })
+        .collect::<Vec<String>>();
+    
+    let result = add_to_table(table_number, menu_item_names).await;
+
+    match result {
+        Ok(orders) => {
+            added_items.extend(orders.iter().map(|o| (table_number, o.id)));
+            println!("Client {} added {} to table {}",
+                client_number,
+                orders.iter().map(|o| o.menu_item_name.to_string()).collect::<Vec<String>>().join(", "),
+                table_number);
+        },
+        Err(e) => println!("Client {} encountered an error trying to add orders to table {}: {}",
+            client_number,
+            table_number,
+            e.to_string())
+    };
 }
 
-async fn list_orders_for_table(table_number: String) {
-    println!("Input an order id or no text to show all orders. Type \"exit\" to go back.");
-    let order_id = get_user_input();
+async fn add_to_table(table_number: u32, menu_item_names: Vec<String>) -> Result<Vec<rest_responses::Order>, String> {
+    let menu_items = client_functions::get_menu_items(HOST.to_string()).await?;
+    let item_ids = menu_item_names
+        .iter()
+        .map(|n| menu_items.menu_items.iter().find(|m| m.name == n.to_string()))
+        .filter(|x| x.is_some())
+        .map(|x| x.unwrap().id)
+        .collect::<Vec<u32>>();
+    client_functions::add_orders(HOST.to_string(), table_number, item_ids, || false).await
+}
 
-    let orders_result = match order_id.trim() {
-        "exit" => return,
-        "" => list_orders_multiple(table_number).await,
-        n => list_orders_single(n.to_string()).await
+async fn delete_random_order(client_number: u32, added_items: &mut Vec<(u32,u32)>) {
+    let table_number = rand::thread_rng().gen_range(1..TABLE_COUNT + 1);
+    let result = match client_functions::get_all_orders(table_number).await {
+        Ok(orders) => delete_random_order_from_table(table_number, orders).await,
+        Err(e) => Err(format!("Client {} encountered an error trying to delete order from table {}: {}",
+            client_number,
+            table_number,
+            e.to_string()))
     };
 
-    let order_text = match orders_result {
-        Result::Ok(orders) =>
-            if orders.len() == 0 {
-                "No orders for this table".to_string()
+    match result {
+        Ok(order_id) => {
+            if let Some(index) = added_items.iter().position(|i| i.0 == table_number && i.1 == order_id) {
+                added_items.swap_remove(index);
             }
-            else {
+            println!("Client {} deleted order {} from table {}.",
+                client_number,
+                order_id,
+                table_number);
+        },
+        Err(e) => println!("Client {} tried to delete a random order from table {}, but encountered an error: {}.",
+            client_number,
+            table_number,
+            e)
+    };
+}
+
+async fn delete_random_order_from_table(table_number: u32, orders: Vec<rest_responses::Order>) -> Result<u32, String> {
+    match orders.choose(&mut rand::thread_rng()) {
+        Some(order) => {
+            client_functions::delete_order(table_number, order.id)?;
+            Ok(order.id)
+        },
+        _ => Err("The table had no orders".to_string())
+    }
+}
+
+async fn query_random_table(client_number: u32) {
+    let table_number = rand::thread_rng().gen_range(1..TABLE_COUNT + 1);
+    match client_functions::get_all_orders(table_number).await {
+        Ok(orders) => {
+            println!("Client {} queried orders for table {}:\r\n{}",
+                client_number,
+                table_number,
                 orders
                     .iter()
-                    .map(|o| format!("{id}: {minutes}", id = o.id, minutes = o.minutes_to_cook))
+                    .map(|o| format!("{}: {}, {} minutes", o.id, o.menu_item_name, o.minutes_to_cook))
                     .collect::<Vec<String>>()
-                    .join("\r\n")
-            },
-        Result::Err(e) => e
+                    .join("\r\n"))
+        },
+        Err(e) => print!("Client {} encountered an error trying to query orders for table {}: {}",
+            client_number,
+            table_number,
+            e.to_string())
     };
-
-    println!("{order_text}");
-    println!("Press enter to continue.");
-    io::stdin().read_line(&mut String::new()).expect("Failed to read line");
 }
 
-async fn list_orders_multiple(table_number: String) -> Result<Vec<rest_responses::Order>, String> {
-    return client_functions::get_all_orders(table_number).await;
-}
-
-async fn list_orders_single(order_id: String) -> Result<Vec<rest_responses::Order>, String> {
-    match client_functions::get_order(order_id).await {
-        Result::Ok(order) => Result::Ok(vec![order]),
-        Result::Err(err) => Result::Err(err.to_string())
-    }
-}
-
-async fn add_orders(menu_items: &Vec<rest_responses::MenuItem>) {
-    println!("Input a table number, or type \"exit\" to go back.");
-    loop {
-        let table_number = get_user_input();
-    
-        match table_number.trim() {
-            "" => {},
-            "exit" => return,
-            n => {
-                add_orders_for_table(n.to_string(), &menu_items).await;
-                return;
-            }
+async fn query_random_table_item(client_number: u32, added_items: &Vec<(u32,u32)>) {
+    if let Some(item_to_query) = added_items.choose(&mut rand::thread_rng()) {
+        match client_functions::get_order(item_to_query.0, item_to_query.1).await {
+            Ok(order) => println!(
+                "Client {} queried order with ID {} for table {}:\r\n{}: {}, {} minutes",
+                client_number,
+                item_to_query.1,
+                item_to_query.0,
+                order.id,
+                order.menu_item_name,
+                order.minutes_to_cook),
+            Err(e) => print!("Client {} encountered an error trying to query a random table order: {}",
+                client_number,
+                e.to_string())
         }
-    }
-}
-
-async fn add_orders_for_table(table_number: String, menu_items: &Vec<rest_responses::MenuItem>) {
-    println!("Input a menu item ID to add an order item. Input no text to confirm. Input \"exit\" to cancel and go back.");
-    println!("Menu item IDs are as follows:");
-
-    let menu_item_text = menu_items
-        .iter()
-        .map(|i| format!("{id}: {name}", id = i.id, name = i.name))
-        .collect::<Vec<String>>()
-        .join("\r\n");
-
-    println!("{menu_item_text}");
-
-    let mut staged_items: Vec<u32> = Vec::new();
-    loop {
-        let menu_item_id = match get_user_input().trim() {
-            "" => break,
-            "exit" => return,
-            id => match id.parse::<u32>() {
-                Ok(id_int) => id_int,
-                Err(_) => continue
-            }
-        };
-        staged_items.push(menu_item_id);
-    }
-
-    if staged_items.len() == 0 {
-        println!("No items added.");
     }
     else {
-        let result = client_functions::add_orders(
-            HOST.to_string(),
-            table_number,
-            staged_items,
-            should_retry).await;
-        let string_result = match result {
-            Result::Ok(()) => "Orders successfully added.".to_string(),
-            Result::Err(e) => e.to_string()
-        };
-    
-        println!("{string_result}");
+        print!("Client {} tried to query a random order but had none to query.", client_number);
     }
-
-    println!("Press enter to continue.");
-    io::stdin().read_line(&mut String::new()).expect("Failed to read line");
-}
-
-fn should_retry() -> bool {
-    println!("The server timed out. Would you like to retry? If not, make sure you check if the item was added afterwords.");
-    println!("Y: retry; N: exit");
-    loop {
-        break match get_user_input().trim() {
-            "Y" => true,
-            "N" => false,
-            _ => continue
-        }
-    }
-}
-
-fn delete_order() {
-    println!("Input a table number, or type \"exit\" to go back.");
-    
-    loop {
-        let table_number = get_user_input();
-
-        match table_number.trim() {
-            "" => {},
-            "exit" => return,
-            id => {
-                delete_order_for_table(id.to_string());
-                return;
-            }
-        }
-    }
-}
-
-fn delete_order_for_table(table_number: String) {
-    println!("Input an order number, or type \"exit\" to go back.");
-
-    loop {
-        let order_number = get_user_input();
-
-        match order_number.trim() {
-            "" => {},
-            "exit" => return,
-            id => {
-                let message = match client_functions::delete_order(table_number, id.to_string()) {
-                    Result::Ok(()) => "Order deleted successfully".to_string(),
-                    Result::Err(e) => e
-                };
-                println!("{message}");
-                println!("Press enter to continue.");
-                io::stdin().read_line(&mut String::new()).expect("Failed to read line");
-                return;
-            }
-        }
-    }
-}
-
-fn get_user_input() -> String {
-    print!("> ");
-    io::stdout().flush().unwrap();
-
-    let mut input = String::new();
-
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-
-    input
 }
