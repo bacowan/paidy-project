f85c7246e049c0d97050480f33017b0db028f8b9
bacowan <bacowan@ualberta.ca>
2024-07-06 20:32:10 +0900

    removed async

--- client/src/client_functions.rs
+++ client/src/client_functions.rs
@@ -7,14 +7,14 @@ use uuid::Uuid;
 use server::{ rest_bodies, rest_responses };
 
 
-pub async fn get_all_orders(host: String, table_number: u32) -> Result<Vec<rest_responses::Order>, String> {
-    let web_response = reqwest::get(format!("{host}/tables/{table_number}/orders"))
-        .await.map_err(|e| e.to_string())?;
+pub fn get_all_orders(host: String, table_number: u32) -> Result<Vec<rest_responses::Order>, String> {
+    let web_response = reqwest::blocking::get(format!("{host}/tables/{table_number}/orders"))
+        .map_err(|e| e.to_string())?;
 
     match web_response.status() {
         StatusCode::OK => {
             let body = web_response.text()
-                .await.map_err(|e| e.to_string())?;
+                .map_err(|e| e.to_string())?;
             Ok(from_str::<rest_responses::Orders>(&body)
                 .map_err(|e| e.to_string())?
                 .orders)
@@ -23,14 +23,14 @@ pub async fn get_all_orders(host: String, table_number: u32) -> Result<Vec<rest_
     }
 }
 
-pub async fn get_order(host: String, table_number: u32, order_id: u32) -> Result<rest_responses::Order, String> {
-    let web_response = reqwest::get(format!("{host}/orders/{order_id}"))
-        .await.map_err(|e| e.to_string())?;
+pub fn get_order(host: String, table_number: u32, order_id: u32) -> Result<rest_responses::Order, String> {
+    let web_response = reqwest::blocking::get(format!("{host}/orders/{order_id}"))
+        .map_err(|e| e.to_string())?;
 
     match web_response.status() {
         StatusCode::OK => {
             let body = web_response.text()
-                .await.map_err(|e| e.to_string())?;
+                .map_err(|e| e.to_string())?;
             from_str::<rest_responses::Order>(&body)
                 .map_err(|e| e.to_string())
         },
@@ -38,7 +38,7 @@ pub async fn get_order(host: String, table_number: u32, order_id: u32) -> Result
     }
 }
 
-pub async fn add_orders<F>(host: String, table_number: u32, menu_item_ids: Vec<u32>, should_retry: F) -> Result<Vec<rest_responses::Order>, String>
+pub fn add_orders<F>(host: String, table_number: u32, menu_item_ids: Vec<u32>, should_retry: F) -> Result<Vec<rest_responses::Order>, String>
                         where F: Fn() -> bool {
     let orders = rest_bodies::Orders {
         orders: menu_item_ids.iter().map(|i| rest_bodies::Order {
@@ -47,14 +47,13 @@ pub async fn add_orders<F>(host: String, table_number: u32, menu_item_ids: Vec<u
         }).collect()
     };
 
-    let client = reqwest::Client::new();
+    let client = reqwest::blocking::Client::new();
 
     let web_response = loop {
         let resp = client.post(format!("{host}/tables/{table_number}/orders"))
             .header("Content-Type", "application/json")
             .body(to_string(&orders).map_err(|e| e.to_string())?)
-            .send()
-            .await;
+            .send();
         let retry = resp
             .as_ref()
             .err()
@@ -67,7 +66,7 @@ pub async fn add_orders<F>(host: String, table_number: u32, menu_item_ids: Vec<u
     match web_response.status() {
         StatusCode::OK => {
             let body = web_response.text()
-                .await.map_err(|e| e.to_string())?;
+                .map_err(|e| e.to_string())?;
             Ok(from_str::<rest_responses::Orders>(&body)
                 .map_err(|e| e.to_string())?
                 .orders)
@@ -80,14 +79,14 @@ pub fn delete_order(table_number: u32, order_id: u32) -> Result<(), String> {
     return Result::Ok(());
 }
 
-pub async fn get_menu_items(host: String) -> Result<rest_responses::MenuItems, String> {
-    let web_response = reqwest::get(format!("{host}/menu-items"))
-        .await.map_err(|e| e.to_string())?;
+pub fn get_menu_items(host: String) -> Result<rest_responses::MenuItems, String> {
+    let web_response = reqwest::blocking::get(format!("{host}/menu-items"))
+        .map_err(|e| e.to_string())?;
 
     match web_response.status() {
         StatusCode::OK => {
             let body = web_response.text()
-                .await.map_err(|e| e.to_string())?;
+                .map_err(|e| e.to_string())?;
             from_str::<rest_responses::MenuItems>(&body)
                 .map_err(|e| e.to_string())
         },
--- client/src/main.rs
+++ client/src/main.rs
@@ -7,7 +7,6 @@ use std::thread;
 use std::time::Duration;
 use rand::Rng;
 use rand::seq::SliceRandom;
-use tokio;
 
 mod client_functions;
 
@@ -16,31 +15,30 @@ const TABLE_COUNT: u32 = 5;
 const TABLET_COUNT: u32 = 1;
 const RUN_TIME_MILLIS: u64 = 60000; // 1 minute
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     for i in 1..TABLET_COUNT + 1 {
-        tokio::task::spawn( async move { client_tablet(i).await; });
+        thread::spawn(move || client_tablet(i));
     }
 
     thread::sleep(Duration::from_millis(RUN_TIME_MILLIS));
     Result::Ok(())
 }
 
-async fn client_tablet(client_number: u32) {
+fn client_tablet(client_number: u32) {
     let mut added_items: Vec<(u32,u32)> = Vec::new();
+    add_random_order(client_number, &mut added_items);
     loop {
-        add_random_order(client_number, &mut added_items).await;
         thread::sleep(Duration::from_millis(rand::thread_rng().gen_range(1000..5000)));
-        /*match rand::thread_rng().gen_range(1..5) {
+        match rand::thread_rng().gen_range(1..5) {
             1 => add_random_order(client_number, &mut added_items),
             2 => delete_random_order(client_number, &mut added_items),
             3 => query_random_table(client_number),
             _ => query_random_table_item(client_number, &added_items)
-        };*/
+        };
     }
 }
 
-async fn add_random_order(client_number: u32, added_items: &mut Vec<(u32,u32)>) {
+fn add_random_order(client_number: u32, added_items: &mut Vec<(u32,u32)>) {
     let table_number = rand::thread_rng().gen_range(1..TABLE_COUNT + 1);
     let menu_item_names = (0..rand::thread_rng().gen_range(1..3))
         .map(|_| match rand::thread_rng().gen_range(1..6) {
@@ -52,7 +50,7 @@ async fn add_random_order(client_number: u32, added_items: &mut Vec<(u32,u32)>) 
         })
         .collect::<Vec<String>>();
     
-    let result = add_to_table(table_number, menu_item_names).await;
+    let result = add_to_table(table_number, menu_item_names);
 
     match result {
         Ok(orders) => {
@@ -69,21 +67,21 @@ async fn add_random_order(client_number: u32, added_items: &mut Vec<(u32,u32)>) 
     };
 }
 
-async fn add_to_table(table_number: u32, menu_item_names: Vec<String>) -> Result<Vec<rest_responses::Order>, String> {
-    let menu_items = client_functions::get_menu_items(HOST.to_string()).await?;
+fn add_to_table(table_number: u32, menu_item_names: Vec<String>) -> Result<Vec<rest_responses::Order>, String> {
+    let menu_items = client_functions::get_menu_items(HOST.to_string())?;
     let item_ids = menu_item_names
         .iter()
         .map(|n| menu_items.menu_items.iter().find(|m| m.name == n.to_string()))
         .filter(|x| x.is_some())
         .map(|x| x.unwrap().id)
         .collect::<Vec<u32>>();
-    client_functions::add_orders(HOST.to_string(), table_number, item_ids, || false).await
+    client_functions::add_orders(HOST.to_string(), table_number, item_ids, || false)
 }
 
-async fn delete_random_order(client_number: u32, added_items: &mut Vec<(u32,u32)>) {
+fn delete_random_order(client_number: u32, added_items: &mut Vec<(u32,u32)>) {
     let table_number = rand::thread_rng().gen_range(1..TABLE_COUNT + 1);
-    let result = match client_functions::get_all_orders(HOST.to_string(), table_number).await {
-        Ok(orders) => delete_random_order_from_table(table_number, orders).await,
+    let result = match client_functions::get_all_orders(HOST.to_string(), table_number) {
+        Ok(orders) => delete_random_order_from_table(table_number, orders),
         Err(e) => Err(e)
     };
 
@@ -104,7 +102,7 @@ async fn delete_random_order(client_number: u32, added_items: &mut Vec<(u32,u32)
     };
 }
 
-async fn delete_random_order_from_table(table_number: u32, orders: Vec<rest_responses::Order>) -> Result<u32, String> {
+fn delete_random_order_from_table(table_number: u32, orders: Vec<rest_responses::Order>) -> Result<u32, String> {
     match orders.choose(&mut rand::thread_rng()) {
         Some(order) => {
             client_functions::delete_order(table_number, order.id)?;
@@ -114,9 +112,9 @@ async fn delete_random_order_from_table(table_number: u32, orders: Vec<rest_resp
     }
 }
 
-async fn query_random_table(client_number: u32) {
+fn query_random_table(client_number: u32) {
     let table_number = rand::thread_rng().gen_range(1..TABLE_COUNT + 1);
-    match client_functions::get_all_orders(HOST.to_string(), table_number).await {
+    match client_functions::get_all_orders(HOST.to_string(), table_number) {
         Ok(orders) => {
             println!("Client {} queried orders for table {}:\r\n{}",
                 client_number,
@@ -134,9 +132,9 @@ async fn query_random_table(client_number: u32) {
     };
 }
 
-async fn query_random_table_item(client_number: u32, added_items: &Vec<(u32,u32)>) {
+fn query_random_table_item(client_number: u32, added_items: &Vec<(u32,u32)>) {
     if let Some(item_to_query) = added_items.choose(&mut rand::thread_rng()) {
-        match client_functions::get_order(HOST.to_string(), item_to_query.0, item_to_query.1).await {
+        match client_functions::get_order(HOST.to_string(), item_to_query.0, item_to_query.1) {
             Ok(order) => println!(
                 "Client {} queried order with ID {} for table {}:\r\n{}: {}, {} minutes",
                 client_number,
